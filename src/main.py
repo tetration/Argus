81d35025cfd68dd8eddec1fac0226b9d4bb4c478
tetration <rafael.august.b@gmail.com>
2024-05-30 16:20:19 -0300

    Fixed bugs which where preventing bot from correctly counting and reseting inacessible websites list and made several improvements on its logic to check which websites are accessible or not

--- src/main.py
+++ src/main.py
@@ -28,35 +28,46 @@ def send_msg(text):
             print(f"Failed to send message: {response.status_code}, {response.reason}")
 
 
-def check_websites(websites, max_attempts=int(os.getenv('max_attempts')), retry_interval=int(os.getenv('retry_interval')), retry_delay=int(os.getenv('retry_delay')), status_report_interval=int(os.getenv('status_report_interval')) ): #Status Report Interval 600 is equal to 10 minutes in real life for 1 hour change it to 3600 seconds
-    unreachable_websites = {}
+def check_websites(websites, max_attempts=int(os.getenv('max_attempts')), retry_interval=int(os.getenv('retry_interval')), retry_delay=int(os.getenv('retry_delay')), status_report_interval=int(os.getenv('status_report_interval')), maximum_retries=int(os.getenv('maximum_retries')) ): #Status Report Interval 600 is equal to 10 minutes in real life for 1 hour change it to 3600
+    attempts_dict_websites = {website: 0 for website in websites}
     accessible_websites = set()
     last_report_time = time.time()
-
+    print(f"attempts dict of websites: {attempts_dict_websites}")
     while True:
+        print(f"attempts dict website: {attempts_dict_websites}")
         for website in websites[:]:
-            attempts = unreachable_websites.get(website, 0)
+            attempts = attempts_dict_websites.get(website, 0)
             if attempts >= max_attempts:
-                print(f"Failed to reach the server: {website} after {attempts} attempts")
-                send_msg(f"Failed to reach the server: {website} after {attempts} attempts")
-                websites.remove(website)
+                print(f"Failed to access {website} after {max_attempts} attempts. {websites} will now be considered inacessible")
+                send_msg(f"Failed to access {website} after {max_attempts} attempts. {websites} will now be considered inacessible")
+                if website in accessible_websites:
+                    accessible_websites.remove(website)
                 continue
 
             try:
                 response = urllib.request.urlopen(website)
-                print(f"{website} is accessible, status code: {response.getcode()}")
-                if website in unreachable_websites:
-                    del unreachable_websites[website]
-                accessible_websites.add(website)
+                if response.getcode() ==200:
+                    print(f"{website} is accessible, status code: {response.getcode()}")
+                    if website in attempts_dict_websites:
+                        attempts_dict_websites[website] = 0
+                    else :
+                        attempts_dict_websites.add()
+                    if website not in accessible_websites:
+                        accessible_websites.add(website)
             except urllib.error.URLError as e:
                 print(f"Attempt {attempts + 1} for {website} failed: {e.reason}")
-                unreachable_websites[website] = attempts + 1
-                if website in accessible_websites:
+                attempts_dict_websites[website] = attempts_dict_websites[website] + 1
+                if website in accessible_websites and attempts_dict_websites[website] == max_attempts:
                     accessible_websites.remove(website)
+                    print(f"Failed to access {website} after {max_attempts} attempts. {websites} will now be considered inacessible")
+                    send_msg(f"Failed to access {website} after {max_attempts} attempts. {websites} will now be considered inacessible")
             except Exception as e:
                 print(f"An error occurred with {website}: {e}")
-                if website in accessible_websites:
+                attempts_dict_websites[website] = attempts_dict_websites[website] + 1
+                if website in accessible_websites and attempts_dict_websites[website] == max_attempts:
                     accessible_websites.remove(website)
+                    print(f"Failed to access {website} after {max_attempts} attempts. {websites} will now be considered inacessible")
+                    send_msg(f"Failed to access {website} after {max_attempts} attempts. {websites} will now be considered inacessible")
 
             time.sleep(retry_interval)
 
@@ -65,37 +76,43 @@ def check_websites(websites, max_attempts=int(os.getenv('max_attempts')), retry_
             print(f"Status report at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(current_time))}:")
             send_msg(f"Status report at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(current_time))}:")
             currentAcessibleWebistes=len(accessible_websites)
-            currentUnreachablewebsites = len(unreachable_websites)
+            currentUnreachablewebsites = sum(1 for attempts in attempts_dict_websites.values() if attempts >= max_attempts)
 
             print(f"Accessible websites:{currentAcessibleWebistes}")
-            send_msg(f"Accessible websites:{currentUnreachablewebsites}")
+            send_msg(f"Accessible websites:{currentAcessibleWebistes}")
             for site in accessible_websites:
                 print(f" - {site}")
                 send_msg(f" - {site}")
             print(f"Inacessible websites:{currentUnreachablewebsites}")
             send_msg(f"Inacessible websites:{currentUnreachablewebsites}")
-            for site in unreachable_websites:
-                print(f" - {site}")
-                send_msg(f" - {site}")
+            for site, attempts in attempts_dict_websites.items():
+                if attempts == max_attempts:
+                    print(f" - {site}")
+                    send_msg(f" - {site}")
             last_report_time = current_time
-
-        if not websites:
-            print("No websites left to check. Moving to retry loop for unreachable websites.")
-            while unreachable_websites:
-                for website, attempts in list(unreachable_websites.items()):
-                    try:
-                        response = urllib.request.urlopen(website)
-                        print(f"{website} is now accessible, status code: {response.getcode()}")
-                        send_msg(f"{website} is now accessible, status code: {response.getcode()}")
-                        del unreachable_websites[website]
-                        websites.append(website)
-                        accessible_websites.add(website)
-                    except urllib.error.URLError as e:
-                        print(f"Retry for {website} failed: {e.reason}")
-                    except Exception as e:
-                        print(f"An error occurred with {website}: {e}")
-
+        # At least one website has surpassed the maximum number of attempts.
+        print("No websites left to check. Moving to retry loop for unreachable websites.")
+        if any(attempts >= max_attempts for attempts in attempts_dict_websites.values()):
+            retries = 0
+            while any(attempts >= max_attempts for attempts in attempts_dict_websites.values()):
+                retries = retries +1
+                for website, attempts in list(attempts_dict_websites.items()):
+                    if attempts_dict_websites[website] == max_attempts:
+                        try: 
+                                response = urllib.request.urlopen(website)
+                                print(f"Looks like {website} is now accessible again, status code: {response.getcode()}")
+                                send_msg(f"Looks like {website} is now accessible again, status code: {response.getcode()}")
+                                attempts_dict_websites[website] = 0
+                                accessible_websites.add(website)
+                        except urllib.error.URLError as e:
+                            print(f"Retry for {website} failed: {e.reason}")
+                        except Exception as e:
+                            print(f"An error occurred with {website}: {e}")
                     time.sleep(retry_delay)
+                if retries <= maximum_retries:# maximum retries will 
+                    current_unreachable_websites = ', '.join(website for website, attempts in attempts_dict_websites.items() if attempts >= max_attempts)
+                    print(f"Superseded maximum retries attempts for currently unreachable websites: {current_unreachable_websites}. Continuing to check other websites.")
+                    break
 
                 current_time = time.time()
                 if current_time - last_report_time >= status_report_interval:
@@ -105,11 +122,19 @@ def check_websites(websites, max_attempts=int(os.getenv('max_attempts')), retry_
                     send_msg(f"Accessible websites:{currentAcessibleWebistes}")
                     for site in accessible_websites:
                         print(f" - {site}")
+                    if all(attempts != max_attempts for attempts in attempts_dict_websites.values()):
+                        print("Currently there are no inacessible websites, looks like everything is healthy!")
+                        send_msg("Currently there are no inacessible websites, looks like everything is healthy!")
+                        break
+                    else :
+                        print(f"Inacessible websites:{currentUnreachablewebsites}")
+                        send_msg(f"Inacessible websites:{currentUnreachablewebsites}")
+                        for site, attempts in attempts_dict_websites.items():
+                            if attempts == max_attempts:
+                                print(f" - {site}")
+                                send_msg(f" - {site}")
                     last_report_time = current_time
 
-                if not unreachable_websites:
-                    print("All previously unreachable websites are now accessible.")
-                    break
 
         else:
             print("Continuing to check the remaining websites.")
